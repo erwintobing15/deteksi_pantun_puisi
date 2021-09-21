5d1294bc74697a77e7ad65b4cd1f6619324533f0
erwintobing15 <erwintobing15@gmail.com>
2021-09-21 22:53:16 +0700

    Create unit test for function get_poem_probability - test if get_poem_probability will return False when received empty poem as parameter - Run test status : OK

--- detection/tests.py
+++ detection/tests.py
@@ -1,12 +1,20 @@
 from django.test import TestCase
-from detection.svm.classifier import get_poem_type
+from detection.svm.classifier import get_poem_type, get_poem_probability
 
 class DetectionTestCase(TestCase):
 
-    def test_get_poem_type_return_empty_string_on_empty_parameter(self):
+    def test_get_poem_type_return_false_on_empty_parameter(self):
         """
         get_poem_type will return False when received empty parameter poem
         """
         poem = []
         poem_type = get_poem_type(poem)
+        self.assertIs(poem_type, False)
+
+    def test_get_poem_probability_return_false_on_empty_parameter(self):
+        """
+        get_poem_probability return False when received empty parameter poem
+        """
+        poem = []
+        poem_type = get_poem_probability(poem)
         self.assertIs(poem_type, False)
