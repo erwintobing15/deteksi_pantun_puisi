e7f5f360a3b9785d174ee573e7af0a3f75329dbd
erwintobing15 <erwintobing15@gmail.com>
2021-09-21 10:08:20 +0700

    Remove js, use backend to display and clear poem

--- detection/views.py
+++ detection/views.py
@@ -5,7 +5,7 @@ def index(request):
     # if there is a POST request we need to process the form data
     if request.method == 'POST':
 
-        poem = request.POST.get('inputPoem')
+        poem = request.POST.get('poemInput')
 
         context = {
             'poem': poem,
