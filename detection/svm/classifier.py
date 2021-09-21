3b7f624f2076ee56664b8552c140f38e078300a2
erwintobing15 <erwintobing15@gmail.com>
2021-09-21 13:05:20 +0700

    Add function get_poem_proba in module classifier

--- detection/svm/classifier.py
+++ detection/svm/classifier.py
@@ -14,3 +14,15 @@ def get_poem_type(poem):
     poem_type = model.predict(vectorized_poem)
 
     return poem_type[0]
+
+def get_poem_probability(poem):
+    """
+    Function to classify input poem as pantun or puisi,
+    received text poem as parameter and return poem type probability
+    """
+    if len(poem) == 0:
+        return False
+    vectorized_poem =  vectorizer.transform([poem])
+    poem_proba = model.predict_proba(vectorized_poem)
+
+    return poem_proba
